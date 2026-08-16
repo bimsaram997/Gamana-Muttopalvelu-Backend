65f7bbac02895b4f7997c8ee050f965579fde32b
Bimsara <bimsara.anaththapathiranage@tuni.fi>
2026-08-16 17:41:11 +0300

    added FE URL

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AngularClientPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200", "https://gamana-muttopalvelu-frontend-dev.onrender.com")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials()
